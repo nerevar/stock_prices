b5f12a609bc6c4864f82d5d114cff0421d61f41e
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2019-03-31 01:05:00 +0300

    MOEX stocks data [2013-2019]

--- download.bash
+++ download.bash
@@ -1,4 +1,4 @@
-time python download_moex_data.py --engine stock --market bonds --date 2016-01-01 --dateend 2017-01-01
-time python download_moex_data.py --engine stock --market shares --date 2016-01-01 --dateend 2017-01-01
-time python download_moex_data.py --engine stock --market index --date 2016-01-01 --dateend 2017-01-01
-time python download_moex_data.py --engine currency --market selt --date 2016-01-01 --dateend 2017-01-01
+time python download_moex_data.py --engine stock --market bonds --date 2013-01-01 --dateend 2014-01-01
+time python download_moex_data.py --engine stock --market shares --date 2013-01-01 --dateend 2014-01-01
+time python download_moex_data.py --engine stock --market index --date 2013-01-01 --dateend 2014-01-01
+time python download_moex_data.py --engine currency --market selt --date 2013-01-01 --dateend 2014-01-01
