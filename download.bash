2de631f6db6b09c2c32a24add64bbad5d60c04d6
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2019-03-30 21:10:03 +0300

    downloader batch

---
+++ download.bash
@@ -0,0 +1,4 @@
+time python download_moex_data.py --engine stock --market bonds --date 2016-01-01 --dateend 2017-01-01
+time python download_moex_data.py --engine stock --market shares --date 2016-01-01 --dateend 2017-01-01
+time python download_moex_data.py --engine stock --market index --date 2016-01-01 --dateend 2017-01-01
+time python download_moex_data.py --engine currency --market selt --date 2016-01-01 --dateend 2017-01-01
