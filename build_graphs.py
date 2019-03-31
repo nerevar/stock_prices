976d0cd772c3353312734a091f0775fc9778b6db
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2019-03-31 13:28:20 +0300

    small refactoring; get date from base script

--- build_graphs.py
+++ build_graphs.py
@@ -18,11 +18,17 @@ def load_graph_builders(graphs_list):
     if graphs_list is None:
         graphs_list = next(os.walk('./graph_data'))[1]
 
-    graphs = {}
-    for folder in graphs_list:
-        if '__' in folder:
+    graphs = defaultdict(list)
+    for graph_name in graphs_list:
+        if '__' in graph_name:
             continue
-        graphs[folder] = load_graph_builder(folder)
+
+        builder = load_graph_builder(graph_name)
+        market_filter = builder.market_filter()
+        graphs[(market_filter['engine'], market_filter['market'])].append({
+            'name': graph_name,
+            'builder': builder
+        })
 
     return graphs
 
@@ -50,12 +56,16 @@ def merge_values(date, value):
     return [date] + (list(value) if isinstance(value, (list, tuple)) else [value])
 
 
-def calc_value(df, builder):
+def get_timestamp(day):
+    # TODO: utc -> moscow time
+    return int(day.timestamp()) * 1000
+
+
+def calc_value(df, builder, day):
     df_filtered = filter_data(df, builder.quote_filter())
     if df_filtered.shape[0] >= 1:
-        date = builder.get_date(df_filtered)
         value = builder.get_value(df_filtered)
-        return merge_values(date, value)
+        return merge_values(get_timestamp(day), value)
     else:
         return None
 
@@ -102,28 +112,19 @@ def parse_args():
 def main(args):
     graph_builders = load_graph_builders(args.graphs)
 
-    results = {}
-    graph_builders_by_market = defaultdict(list)
-    for name, builder in graph_builders.items():
-        results[name] = []
-        market_filter = builder.market_filter()
-        graph_builders_by_market[(market_filter['engine'], market_filter['market'])].append({
-            'name': name,
-            'builder': builder
-        })
-
     if args.dateend:
         dates = daterange(args.date, args.dateend)
     else:
         dates = [args.date]
 
+    results = defaultdict(list)
     for day in dates:
-        for (engine, market), builders in graph_builders_by_market.items():
+        for (engine, market), builders in graph_builders.items():
             data = load_data(engine, market, day)
             if data is None:
                 continue
             for builder in builders:
-                result = calc_value(data, builder['builder'])
+                result = calc_value(data, builder['builder'], day)
                 if result is not None:
                     results[builder['name']].append(result)
 
--- graph_data/moex/graph.py
+++ graph_data/moex/graph.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-
 class GraphBuilder(object):
     def market_filter(self):
         return {
@@ -11,15 +8,9 @@ class GraphBuilder(object):
     def quote_filter(self):
         return {
             'BOARDID': 'SNDX',
-            'SECID': 'IMOEX' # TODO: массив значений для индекса
+            'SECID': 'IMOEX'
         }
 
-    def get_date(self, df):
-        quote = df.iloc[0]
-        dt = datetime.strptime(quote.get('TRADEDATE'), '%Y-%m-%d')
-        return int(dt.timestamp()) * 1000
-
     def get_value(self, df):
-        # TODO: на вход принимать df с одним или несколькими рядами
         quote = df.iloc[0]
         return float(quote.get('CLOSE'))
